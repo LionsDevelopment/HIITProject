fa93a5483ee618584878e204004791fb783fa913
ll030482 <ll030482@sdale.org>
2020-11-10 13:59:30 -0600

    Editiing to activity_create_play_list.xml including coloured text in spinners.

--- app/src/main/java/com/example/hiitproject/CreatePlayList.java
+++ app/src/main/java/com/example/hiitproject/CreatePlayList.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -16,17 +18,33 @@ public class CreatePlayList extends AppCompatActivity {
         }
         catch(NullPointerException e){}
     }
-
+    public void adaptArrayIntoSpinner(Spinner spin, int arrloc){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, arrloc, R.layout.spinner_custom);
+        adapter.setDropDownViewResource(R.layout.spinner_custom);
+        spin.setAdapter(adapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_play_list);
-        hideTopAction();
+        //Variables
         Intent createin = getIntent();
-        EditText plltitle = (EditText)findViewById(R.id.plltitle);
-        Spinner spinday = (Spinner)findViewById(R.id.dayspin);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.timeofday, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinday.setAdapter(adapter);
+        EditText plltitle = findViewById(R.id.plltitle);
+        Spinner spintime = findViewById(R.id.timespin);
+        Spinner spinday = findViewById(R.id.dayspin);
+        Button backtohome = findViewById(R.id.backbutton);
+
+        //Actions
+        hideTopAction();
+        adaptArrayIntoSpinner(spintime, R.array.timeofday);
+        adaptArrayIntoSpinner(spinday, R.array.dayofweek);
+        backtohome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backhomein = new Intent(CreatePlayList.this, HomePage.class);
+                startActivity(backhomein);
+            }
+        });
+
     }
 }
